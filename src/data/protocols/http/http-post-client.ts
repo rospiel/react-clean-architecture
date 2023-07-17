97390e07c0906acdcbde076fa899aa5c7fefe1d7
rospiel <rospielberg@gmail.com>
2023-07-17 18:51:29 -0300

    add authentication data and domain layer

---
+++ src/data/protocols/http/http-post-client.ts
@@ -0,0 +1,10 @@
+import { HttpResponse } from '.'
+
+export type HttpPostClientParams<T> = {
+  url: string
+  body?: T
+}
+
+export interface HttpPostClient<T, R> {
+  post (params: HttpPostClientParams<T>): Promise<HttpResponse<R>>
+}
---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,28 @@
+import { HttpPostClient, HttpPostClientParams } from '@/data/protocols/http'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
+import { AuthenticationParams, Authentication } from '@/domain/usecases'
+
+export class RemoteAuthentication implements Authentication {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
+  ) { }
+
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    const httpPostClientParams: HttpPostClientParams<AuthenticationParams> = {
+      url: this.url,
+      body: params
+    }
+    const response = await this.httpPostClient.post(httpPostClientParams)
+
+    switch (response.statusCode) {
+      case HttpStatusCode.ok: return response.body
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
+    }
+  }
+}
