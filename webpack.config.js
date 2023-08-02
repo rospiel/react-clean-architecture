1db7421c603acecf3786fb1717b07242738524c1
rospiel <rospielberg@gmail.com>
2023-08-02 18:01:13 -0300

    chore.: extra configs react and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,14 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', 'tsx', '.js', 'scss'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
   module: {
     rules: [{
-      test: /\.scss$/,
+      test: /\.ts(x?)$/,
       loader: 'ts-loader',
       exclude: /node_modules/
     }, {
