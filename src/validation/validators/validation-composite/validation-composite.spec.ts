a563570569471f5a3639043c79b87d3643433e97
rospiel <rospielberg@gmail.com>
2023-08-24 18:49:10 -0300

    feat.: add composite pattern and using withoud build pattern

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,39 @@
+import { FieldValidationSpy } from '../test/mock-field-validation'
+import { ValidationComposite } from './validation-composite'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: ValidationComposite
+  listFieldValidationSpy: FieldValidationSpy[]
+}
+
+function makeSut (fieldName: string): SutTypes {
+  const fieldValidationSpy = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+
+  const sut = new ValidationComposite(fieldValidationSpy)
+  return { sut, listFieldValidationSpy: fieldValidationSpy }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut, listFieldValidationSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    listFieldValidationSpy[0].error = new Error(errorMessage)
+    listFieldValidationSpy[1].error = new Error(faker.random.words())
+
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(error)
+  })
+
+  test('Should not return error when value success validated', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
+  })
+})
