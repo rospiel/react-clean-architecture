a80a32053f160e1bcea80e8aeb7d0f64c3a39040
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2024-02-15 20:01:34 +0000

    Deploy rospiel/react-clean-architecture to github.com/rospiel/react-clean-architecture.git:gh-pages

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = merge(common, {
       }]
     },
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
+      contentBase: path.join(__dirname, 'docs'),
       writeToDisk: true,
       historyApiFallback: true
     },
