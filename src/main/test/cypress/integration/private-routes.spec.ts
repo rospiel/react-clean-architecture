13642d3e0860fb3aa5101225b521d54071d3e384
Rodrigo Santos <rospielberg@gmail.com>
2024-02-20 15:51:59 -0300

    test.: add test for new route

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,8 +1,13 @@
 import * as Helper from '../utils/helpers'
 
 describe('Private routes', function () {
-    it('Should logout when without a token', function () {
+    it('Should logout when without a token on main page', function () {
         cy.visit('')
+        Helper.testUrl('/login')
+    })
+
+    it('Should logout when without a token on survey result page', function () {
+        cy.visit('/surveys/id')
         Helper.testUrl('/login')
     })
 })
